1f54398e1ca08039e3338308da9bacf4e3b9589e
TammyBriggs <t.briggs1@alustudent.com>
2024-07-06 21:37:43 +0000

    retry

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,17 +5,23 @@ Contains the number_of_subscribers function
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    r = requests.get(
-        'http://www.reddit.com/r/{}/about.json'.format(subreddit),
-        headers={
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-                          "AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/126.0.0.0 Safari/537.36"
-        }).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/126.0.0.0 Safari/537.36"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    except requests.RequestException:
+        return 0
